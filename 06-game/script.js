59dc0a4332f8fb475610e96a00b7fea9b3f8ceee
suniaikin <class.suniaikin@gmail.com>
2025-08-17 21:59:18 -0700

    game logic

--- 06-game/script.js
+++ 06-game/script.js
@@ -2,3 +2,33 @@ let userChoice
 let computerChoice
 let isWinner = false
 
+while (!isWinner) {
+	userChoice = prompt('Выбери камень, ножницы или бумага:')
+	userChoice = userChoice.toLowerCase()
+
+	let randomNumber = Math.floor(Math.random() * 3)
+
+	if (randomNumber === 0) {
+		computerChoice = 'камень'
+	} else if (randomNumber === 1) {
+		computerChoice = 'ножницы'
+	} else {
+		computerChoice = 'бумага'
+	}
+
+	if (userChoice === computerChoice) {
+		alert('Ничья! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+	} else if (
+		(userChoice === 'камень' && computerChoice === 'ножницы') ||
+		(userChoice === 'ножницы' && computerChoice === 'бумага') ||
+		(userChoice === 'бумага' && computerChoice === 'камень')
+	) {
+		alert('Ты выиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+		isWinner = true
+	} else {
+		alert('Ты проиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+	}
+
+
+
+}
