1dabbabc033a9b6c1d31bc2e783fbb8deceaba5d
suniaikin <class.suniaikin@gmail.com>
2025-08-17 22:03:57 -0700

    logic 1

--- 06-game/script.js
+++ 06-game/script.js
@@ -16,18 +16,29 @@ while (!isWinner) {
 		computerChoice = 'бумага'
 	}
 
-	if (userChoice === computerChoice) {
-		alert('Ничья! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
-	} else if (
-		(userChoice === 'камень' && computerChoice === 'ножницы') ||
-		(userChoice === 'ножницы' && computerChoice === 'бумага') ||
-		(userChoice === 'бумага' && computerChoice === 'камень')
-	) {
-		alert('Ты выиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
-		isWinner = true
+	if (userChoice === 'камень' || userChoice === 'ножницы' || userChoice === 'бумага') {
+
+
 	} else {
-		alert('Ты проиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+		alert('Пожалуйста, введи корректный ход: камень, ножницы или бумага.')
 	}
+
+
+
+
+
+	// if (userChoice === computerChoice) {
+	// 	alert('Ничья! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+	// } else if (
+	// 	(userChoice === 'камень' && computerChoice === 'ножницы') ||
+	// 	(userChoice === 'ножницы' && computerChoice === 'бумага') ||
+	// 	(userChoice === 'бумага' && computerChoice === 'камень')
+	// ) {
+	// 	alert('Ты выиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+	// 	isWinner = true
+	// } else {
+	// 	alert('Ты проиграл! Ты выбрал ' + userChoice + ', компьютер выбрал ' + computerChoice)
+	// }
 
 
 
