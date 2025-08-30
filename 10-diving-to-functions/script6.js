0a7bae6a788e693139a614dff37196477124ae6a
suniaikin <class.suniaikin@gmail.com>
2025-08-29 23:37:59 -0700

    solution2

--- 10-diving-to-functions/script6.js
+++ 10-diving-to-functions/script6.js
@@ -41,11 +41,10 @@ const favoriteBooks = [
   },
 ]
 
-const printCheapBooks  = (books) => {
-	for (let i = 0; i < books.length; i++) {
-		if (books[i]["price"] <= 30) {
-			console.log(`Price of ${books[i]["title"]} is ${books[i]["price"]}`)
-		}
-	}
-}
-printCheapBooks(favoriteBooks)
+const printCheapBooks = (books) => {
+  for (const book of books) {
+    if (book.price <= 30) {
+      console.log(`Цена "${book.title}" составляет ${book.price}`);
+    }
+  }
+};
