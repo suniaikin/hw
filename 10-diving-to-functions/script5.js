bb7ce808eaaac1e898adcd0de6221a997c1e5271
suniaikin <class.suniaikin@gmail.com>
2025-08-29 23:01:30 -0700

    callbacks

---
+++ 10-diving-to-functions/script5.js
@@ -0,0 +1,47 @@
+// Callbacks
+// Теперь напишем функцию, которая принимает массив книг
+// и выводит в консоль цену и названия дешёвых книг (стоимостью меньше 30).
+// Будем использовать синтаксис стрелочных функций.
+
+const favoriteBooks = [
+  {
+    id: 1,
+    title: 'Head First JavaScript Programming',
+    author: 'Eric Freeman',
+    price: 29.99,
+    genre: 'Programming',
+  },
+  {
+    id: 2,
+    title: 'You Don’t Know JS',
+    author: 'Kyle Simpson',
+    price: 39.99,
+    genre: 'Programming',
+  },
+  {
+    id: 3,
+    title: 'JavaScript: The Definitive Guide',
+    author: 'David Flanagan',
+    price: 49.99,
+    genre: 'Programming',
+  },
+  {
+    id: 4,
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    price: 29.99,
+    genre: 'Fantasy',
+  },
+  {
+    id: 5,
+    title: 'The Witcher',
+    author: 'Andrzej Sapkowski',
+    price: 19.99,
+    genre: 'Fantasy',
+  },
+]
+
+
+
+
+printCheapBooks(favoriteBooks)
