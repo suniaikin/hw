4ed71cb98e3d13b947505456b563f38358568e1c
suniaikin <class.suniaikin@gmail.com>
2025-08-30 17:05:41 -0700

    arrow function

--- 10-diving-to-functions/script1.js
+++ 10-diving-to-functions/script1.js
@@ -3,3 +3,18 @@ function sumNumbers(a, b) {
 	return a + b
 }
 
+// Function Expression
+const fakeName = function (a, b) {
+	return a + b
+}
+
+console.log(sumNumbers(2, 2))
+console.log(fakeName(2, 2))
+
+// Arrow function
+//Задаются только через Function Expression
+// let variable = () => {}
+
+const arrowFunc = (a, b) => a + b
+
+console.log(arrowFunc(2, 5))
