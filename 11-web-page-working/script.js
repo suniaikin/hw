48eac89f07e48cd86412875b07e61d92151eea3c
suniaikin <class.suniaikin@gmail.com>
2025-09-08 08:05:36 -0700

    train

--- 11-web-page-working/script.js
+++ 11-web-page-working/script.js
@@ -1,3 +1,6 @@
-console.log(window.document.body.children[0])
-console.log(window.document.body.children[2])
-console.log(window.document.body.children[1].children[0])
+// console.log(window.document.body.children[0].classList)
+// console.log(window.document.body.children[2])
+// console.log(window.document.body.children[1].children[0].textContent)
+
+const listItem = document.createElement('li')
+listItem.textContent = 'Item 4'
