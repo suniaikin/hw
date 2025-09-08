14f9ee01cebeaadd55aaab622a3fc3a56bba5fdb
suniaikin <class.suniaikin@gmail.com>
2025-09-08 08:34:58 -0700

    create new element

--- 11-web-page-working/script.js
+++ 11-web-page-working/script.js
@@ -1,9 +1,12 @@
-// console.log(window.document.body.children[0].classList)
-// console.log(window.document.body.children[2])
-// console.log(window.document.body.children[1].children[0].textContent)
+// // console.log(window.document.body.children[0].classList)
+// // console.log(window.document.body.children[2])
+// // console.log(window.document.body.children[1].children[0].textContent)
+// const listItem = document.createElement('li')
+// listItem.classList.add('item')
+// listItem.textContent = 'Item 4'
+// const list = document.querySelector('.list')
+// list.prepend(listItem)
 
-const listItem = document.createElement('li')
-listItem.classList.add('item')
-listItem.textContent = 'Item 4'
-document.querySelector('ul').append(listItem)
+//Create new element
+const neElement = document.createElement('div')
 
