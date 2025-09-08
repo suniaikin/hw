9311da3483d9df0053326a09c8eb2a6f71719778
suniaikin <class.suniaikin@gmail.com>
2025-09-08 08:18:09 -0700

    train

--- 11-web-page-working/script.js
+++ 11-web-page-working/script.js
@@ -3,4 +3,7 @@
 // console.log(window.document.body.children[1].children[0].textContent)
 
 const listItem = document.createElement('li')
+listItem.classList.add('item')
 listItem.textContent = 'Item 4'
+document.querySelector('ul').append(listItem)
+
