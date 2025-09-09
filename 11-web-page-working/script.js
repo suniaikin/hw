48ea72bddc7fedc71dc188edca295b6a5e7be1c9
suniaikin <class.suniaikin@gmail.com>
2025-09-09 07:00:01 -0700

    search

--- 11-web-page-working/script.js
+++ 11-web-page-working/script.js
@@ -13,6 +13,17 @@ const neElement = document.createElement('div')
 //Add an element to the page
 document.body.append(neElement)
 
+//Search for an element on the page
+const titleElement = document.getElementById('id-элемента')
 
+//Search for an element on the page by class
+const header = document.getElementsByTagName("h1")
+
+//Search for an element on the page
+const listItem = document.querySelector('.list .current') //first element
+
+//Search for all elements on the page
+const listItems = document.querySelectorAll('ol .item') //all elements
+console.log(listItems) //NodeList
 
 
