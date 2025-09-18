04df14286a8b3997428550ed04cfa9bd289de09e
suniaikin <class.suniaikin@gmail.com>
2025-09-17 18:17:02 -0700

    learning

--- 08-objects/script.js
+++ 08-objects/script.js
@@ -22,3 +22,24 @@ console.log(car.brend);
 console.log(car["brend"]);
 console.log(car.engine.type);
 console.log(car.engine);
+console.log(car.prev_owners);
+console.log(car.prev_owners[0]);
+
+
+// создание нового свойства (инициализация свойства значением)
+car.lastPrice = 14500
+
+// изменение значения свойства (переприсваивание)
+car.millage = 250000
+
+// удаление свойства из объекта
+delete car.isTaxi
+
+
+// все ключи объекта
+const keys = Object.keys(car)
+console.log(keys);
+
+// все значения объекта
+const engine = Object.values(car.engine)
+console.log(engine);
