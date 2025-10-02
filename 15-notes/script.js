d03b1456a7be7279bbfc6aa0f5a2cfe763e1460c
suniaikin <class.suniaikin@gmail.com>
2025-10-01 23:38:07 -0700

    js

---
+++ 15-notes/script.js
@@ -0,0 +1,10 @@
+// находим главный контейнер с id 'app'
+const app = document.getElementById('app');
+
+// функция для рендеринга интерфейса
+function render() {
+	app.innerHTML = ''; // очищаем содержимое контейнера
+	console.log('render'); // логируем рендеринг
+
+
+}
