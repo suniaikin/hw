8941204e285ed1e042b2cbfe287713e6cb68103b
suniaikin <class.suniaikin@gmail.com>
2025-10-04 21:30:33 -0700

    lesson

--- 16-notes/script.js
+++ 16-notes/script.js
@@ -1,29 +1,53 @@
 // model
-
 const model = {
 	notes: [
 		{
 			id: 1,
-			title: 'Flexbox (CSS)',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			color: '#fcca78', // Желтый
-			isFavorite: true
+			title: "Flexbox (CSS)",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			color: "#fcca78", // Желтый
+			isFavorite: true,
 		},
 		{
 			id: 2,
-			title: 'Объекты (JavaScript)',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			color: '#b2e597', // Зеленый
-			isFavorite: false
-		}
-	]
-}
+			title: "Объекты (JavaScript)",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			color: "#b2e597", // Зеленый
+			isFavorite: false,
+		},
+	],
+};
 
 // view
-
 const view = {
-	renderNotes() {
-		const root = document.getElementById('root'); // обращение к корневому элементу 
-		root.innerHTML = ''; // очистка корневого элемента
-	}
-}
+	  createNoteElement(note) {
+		  const newDiv = document.createElement("div");
+		  newDiv.classList.add("note");
+		  newDiv.dataset.color = note.color;
+   },
+
+	renderNotes(notes) {
+		const root = document.getElementById("root");
+		root.innerHTML = "";
+
+		//пустой черновик
+		const fragment = document.createDocumentFragment();
+		
+		notes.forEach((note) => {
+			const noteElement = this.createNoteElement(note);
+			fragment.appendChild(noteElement);
+			
+		})
+
+	},
+};
+
+// controller
+
+const controller = {
+	init() {
+		view.renderNotes(model.notes);
+	},
+};
+
+controller.init();
