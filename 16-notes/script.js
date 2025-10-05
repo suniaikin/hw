bc60d30f69163e4bc5e83a0f26d6010bbd4a1c61
suniaikin <class.suniaikin@gmail.com>
2025-10-04 23:35:54 -0700

    forEach

--- 16-notes/script.js
+++ 16-notes/script.js
@@ -23,24 +23,34 @@ const model = {
 
 const view = {
 
+
+
+	// функция для создания элемента заметки
 	createNoteElement(note) {
 		const newDiv = document.createElement("div");
 		newDiv.classList.add("note");
 		newDiv.dataset.color = note.color;
 	},
 
+
+
+	// функция для рендеринга всех заметок
 	renderNotes(notes) {
 		const root = document.getElementById("root");
 		root.innerHTML = "";
 
-		//пустой черновик
+		// создаем фрагмент для оптимизации
 		const fragment = document.createDocumentFragment();
 
+		// перебираем массив заметок и создаем для каждой заметки элемент
 		notes.forEach((note) => {
 			const noteElement = this.createNoteElement(note);
-			fragment.appendChild(noteElement);
+			fragment.append(noteElement);
 
 		})
+
+		// добавляем фрагмент в корневой элемент за 1 раз
+		root.appendChild(fragment);
 
 	},
 
